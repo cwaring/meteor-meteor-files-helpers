ac5e337646b05e09a56b992f95b0ee9904db4538
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-06-01 09:30:29 +0200

    Adds git url to package description and removes package description comments

--- src/package.js
+++ src/package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'sanjo:meteor-files-helpers',
   version: '1.1.0_4',
-  // Brief, one-line summary of the package.
   summary: 'Helpers for interacting with Meteor files in the filesystem',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/Sanjo/meteor-meteor-files-helpers.git',
   documentation: 'README.md'
 })
 
